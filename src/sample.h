b26de0cdf416e2bdce33e785f355a0afa4509af0
zout3 <83205716+zout3@users.noreply.github.com>
2022-11-24 13:09:37 -0800

    Delete src directory

--- src/sample.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <armadillo>
-
-//Modified sample_main from RcppArmadilloExtensions/sample.h
-
-arma::uvec sample(const arma::uvec& x, const int size, const arma::vec& prob);
-
-arma::uvec sample(const arma::uvec& x, const int size);
